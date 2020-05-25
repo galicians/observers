ca0419a0291f3b045c08cd27c740fa605b7d0352
galicians <galicians@gmail.com>
2020-05-25 11:38:37 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import { of } from 'rxjs'; 
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 const source = of('World').pipe(
   map(x => `Hello ${x}!`)
 );
-
 source.subscribe(x => console.log(x));
+
+const observer = {
+  next: apple => console.log(`Apple was emitted ${apple}`),
+  error: err => console.log(`Error ocurred: ${err}`),
+  complete: () => console.log(`No more apples, go home`)
+};
+
+const appleStream = new Observable(appleObserver => {
+  appleObserver.next('Apple 1');
+  appleObserver.next('Apple 2');
+  appleObserver.complete();
+});
+
+const sub = appleStream.subscribe(observer)
